388bc8ab460d5d41d72adc2697bb4935ca84e363
RafiaHriti <rafianazhat@gmail.com>
2024-03-03 11:12:09 +0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,11 +11,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="winter">
       <body className={inter.className}>{children}</body>
     </html>
   );
