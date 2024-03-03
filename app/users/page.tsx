6be1952db078c918cc84cc6e933bdf94741e812e
RafiaHriti <rafianazhat@gmail.com>
2024-03-03 11:16:10 +0600

    Moved user table to its own component on the same path

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,47 +1,14 @@
 import { link } from "fs";
 import React from "react";
-
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
+import UserTable from "./UserTable";
 
 const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    /**
-     * If we don't want Next to cached response from the API, enable the config below, and disable the bottom.
-     * Without the config, if Next assumes that the data is static, it will also generate a static HTML for the page
-     * automatically instead of generating a dynamically generated HTML.
-     */
-    // cache: 'no-store'
-    next: {
-      revalidate: 10, // Revalidates response from the mentioned API after every 10 seconds.
-    },
-  });
-  const users: User[] = await res.json();
-
   return (
     <>
       <h1>User Page</h1>
       {/* <p>{new Date().toLocaleTimeString()}</p> */}
 
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>User Name</th>
-            <th>User Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable />
     </>
   );
 };
