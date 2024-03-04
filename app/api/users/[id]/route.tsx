dc430ad3fb90b8fba6b8f82fe3c1d975e1494186
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-04 15:31:15 +0600

    Created an API route for updating a user

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -13,3 +13,26 @@ export function GET(
   // Else return the data
   return NextResponse.json({ id: 1, name: "Fuad" });
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Validate the request body
+  const body = await request.json();
+
+  // If invalid, return 400 error
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  // Fetch the user with the given id
+
+  // If the user does not exist, return 404
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  // Update the user in the database
+
+  // Return the updated user
+  return NextResponse.json({ id: params.id, name: body.name });
+}
