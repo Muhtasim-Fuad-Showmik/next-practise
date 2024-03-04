d46ebccf3ab718f6aeaf0bdedfc3db62360cdf96
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-05 02:51:01 +0600

    Added image upload and display logic

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,16 +1,38 @@
 "use client";
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
   return (
-    <CldUploadWidget uploadPreset="xtqdtanu">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={180}
+          alt="uploaded image"
+        ></CldImage>
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="xtqdtanu"
+        onSuccess={(result, options) => {
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 
