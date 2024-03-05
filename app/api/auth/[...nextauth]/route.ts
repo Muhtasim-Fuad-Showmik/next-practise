cb3994c88dbcde3f4886b8cbcb498bda66a2707f
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-05 14:23:40 +0600

    Used server side sessions to display user name on the home page

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,13 +3,15 @@ import GoogleProvider from "next-auth/providers/google";
 
 // Generated client ID and client secret from:
 // ("https://console.cloud.google.com/apis/credentials?authuser=1&project=next-practice-25188&supportedpurview=project");
-const handler = NextAuth({
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
