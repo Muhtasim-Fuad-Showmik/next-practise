5020959dc1eed9e616e47df00091f8f6420bc4c7
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-05 16:04:17 +0600

    Updated auth options strategy for jwt tokens instead of unsupported database sessions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,21 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
 
 // Generated client ID and client secret from:
 // ("https://console.cloud.google.com/apis/credentials?authuser=1&project=next-practice-25188&supportedpurview=project");
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 };
 
 const handler = NextAuth(authOptions);
