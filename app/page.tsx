da52d43a5e3b011343d6b17f120e8ee3b1972aec
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-05 23:29:40 +0600

    Loaded images onto the application

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import neuvilette from "@/public/images/Neuvilette.png";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard/ProductCard";
 import { getServerSession } from "next-auth";
@@ -8,10 +9,21 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
 
   return (
-    <main>
+    <main className="relative h-screen">
       <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
+
+      <Image src={neuvilette} alt="Neuvilette Splash Art" className="mt-3" />
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="Coffee"
+        width={300}
+        height={170}
+        className="mt-3 object-cover"
+        quality={100}
+        priority
+      />
     </main>
   );
 }
