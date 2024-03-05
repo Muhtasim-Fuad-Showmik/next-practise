9292546b31b39f53fee55ce86411b1aae036c75d
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-03-05 23:49:57 +0600

    Added metadata to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import { Metadata } from "next";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -27,3 +28,8 @@ export default async function Home() {
     </main>
   );
 }
+
+export const metadata: Metadata = {
+  title: "Home Page",
+  description: "First page that the user comes into",
+};
